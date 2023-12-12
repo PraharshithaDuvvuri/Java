0cb7d9aa27c13c6fce80b8a8921f26569f32f2c0
PraharshithaDuvvuri <127878556+PraharshithaDuvvuri@users.noreply.github.com>
2023-12-12 14:58:00 +0530

    changes done to classandobjectimplementation.java

--- Downloads/Java/classandobjectimplementation.java
+++ Downloads/Java/classandobjectimplementation.java
@@ -1,4 +1,5 @@
 class classandobjectimplementation {
+    //Real-world objects share two characteristics: They all have state and behavior. Dogs have state (name, color, breed, hungry) and behavior (barking, fetching, wagging tail). Bicycles also have state (current gear, current pedal cadence, current speed) and behavior (changing gear, changing pedal cadence, applying brakes). Identifying the state and behavior for real-world objects is a great way to begin thinking in terms of object-oriented programming.In the real world, you'll often find many individual objects all of the same kind. There may be thousands of other bicycles in existence, all of the same make and model. Each bicycle was built from the same set of blueprints and therefore contains the same components. In object-oriented terms, we say that your bicycle is an instance of the class of objects known as bicycles. A class is the blueprint from which individual objects are created.
     int speed=0;
     int cadence=0;
     int gear=1;
